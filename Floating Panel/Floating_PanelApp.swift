126e6333848c8ed86f1b7dc6d0bdad0b4904a67a
Mohamed-Mostafa7 <mohameds.mostafas7@gmail.com>
2024-09-10 16:21:19 +0300

    Initial Commit

---
+++ Floating Panel/Floating_PanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Floating_PanelApp.swift
+//  Floating Panel
+//
+//  Created by Mohamed Mostafa on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Floating_PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
